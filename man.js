51a2ecee82f85cae5e124fd580bb9875192d669f
Lucas-Bispo <lukaobispo@gmail.com>
2022-11-30 11:08:04 -0300

    :truck: Mova ou renomeie recursos (por exemplo: arquivos, caminhos, rotas)

--- man.js
+++ man.js
@@ -9,46 +9,48 @@ const firebaseConfig = {
 };
 
 
+  // inicializar o firebase
+  firebase.initializeApp(firebaseConfig);
   
- // inicializar o firebase
- firebase.initializeApp(firebaseConfig);
+  // referenciar seu database
+  var contactFormDB = firebase.database().ref("contactForm");
   
- // referenciar seu database
- var contactFormDB = firebase.database().ref("contactForm");
- 
- document.getElementById("contactForm").addEventListener("submit", submitForm);
- 
- function submitForm(e) {
-   e.preventDefault();
- 
-   var name = getElementVal("name");
-   var emailid = getElementVal("emailid");
-   var msgContent = getElementVal("msgContent");
- 
-   saveMessages(name, emailid, msgContent);
- 
-   //   alerta 
-   document.querySelector(".alert").style.display = "block";
- 
-   //   remove o alerta do formulario 
-   setTimeout(() => {
-     document.querySelector(".alert").style.display = "none";
-   }, 3000);
- 
-   //   reset o formulario 
-   document.getElementById("contactForm").reset();
- }
- 
- const saveMessages = (name, emailid, msgContent) => {
-   var newContactForm = contactFormDB.push();
- 
-   newContactForm.set({
-     name: name,
-     emailid: emailid,
-     msgContent: msgContent,
-   });
- };
- 
- const getElementVal = (id) => {
-   return document.getElementById(id).value;
- };
+  document.getElementById("contactForm").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+  
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var dateid = getElementVal("dateid");
+    var msgContent = getElementVal("msgContent");
+    
+  
+    saveMessages(name, emailid, dateid, msgContent);
+  
+    //   alerta 
+    document.querySelector(".alert").style.display = "block";
+  
+    //   remove o alerta do formulario 
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 3000);
+  
+    //   reset o formulario 
+    document.getElementById("contactForm").reset();
+  }
+  
+  const saveMessages = (name, emailid, dateid, msgContent) => {
+    var newContactForm = contactFormDB.push();
+  
+    newContactForm.set({
+      name: name,
+      emailid: emailid,
+      dateid: dateid,
+      msgContent: msgContent,
+    });
+  };
+  
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
